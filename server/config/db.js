ef04aaa7531a9ba3a6a7f31769751109e22da3af
Quyet <20020467@vnu.edu.vn>
2023-04-12 23:36:38 +0700

    config

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,16 @@
-const {
-    Sequelize
-} = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const host = process.env.HOST;
 const database = process.env.DATABASE;
 const username = process.env.USER;
 const password = process.env.PASSWORD;
-const port = process.env.DB_PORT
+const port = process.env.DB_PORT;
 
 const sequelize = new Sequelize(database, username, password, {
-    host,
-    port,
-    dialect: 'mysql',
-    logging: false,
+  host,
+  port,
+  dialect: "mysql",
+  logging: false,
 });
 
 module.exports = sequelize;
